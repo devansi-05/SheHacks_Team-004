b9fc2dde46bd1a44ef13a7eb2eeca8c96eec092f
cyraalesha <67356675+cyraalesha@users.noreply.github.com>
2021-03-13 19:05:25 +0700

    Add sign up

--- app.js
+++ app.js
@@ -51,6 +51,26 @@ function login(){
 
 }
 
+function signup(){
+
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+
+}
+
 function logout(){
   firebase.auth().signOut();
 }
